d884c6741ab78d72e100d082bb6811a05abe7f7d
George Gaskin <george_gaskin@msn.com>
2021-07-05 23:43:18 +0200

    Backup

--- src/app/asteroidsGame.ts
+++ src/app/asteroidsGame.ts
@@ -1,34 +1,36 @@
-
 import P5 from "p5";
 import { sketch } from "./p5-sketch";
 
 export class AsteroidsGame {
-  _angle:number=0;
+  _shipHeading: number = 0;
 
   constructor() {
     new P5((p5) => sketch(p5, this.setup));
   }
 
-  public setup=(p5: P5)=> {
-    p5.draw=()=>this.gameLoop(p5);
-
+  public setup = (p5: P5) => {
     // Creating and positioning the canvas
-    const scr_reduction=0.8;
-    const canvas = p5.createCanvas(p5.windowWidth*scr_reduction, p5.windowHeight*scr_reduction);
+    const scr_reduction = 0.8;
+    const canvas = p5.createCanvas(
+      p5.windowWidth * scr_reduction,
+      p5.windowHeight * scr_reduction
+    );
     canvas.parent("app");
-  }
 
-  public gameLoop=(p5: P5)=> {
+    p5.draw = () => this.gameLoop(p5);
+  };
+
+  public gameLoop = (p5: P5) => {
     p5.background(0);
-    
+
     //advance rotation angle
-    this._angle+=p5.PI/100;
-    
+    this._shipHeading += p5.PI / 100;
+
     //Draw the spaceship
     p5.noFill();
-    p5.stroke('white');
+    p5.stroke("white");
     p5.translate(p5.width / 2, p5.height / 2);
-    p5.rotate(this._angle)
+    p5.rotate(this._shipHeading);
     p5.scale(2);
 
     p5.beginShape();
@@ -38,6 +40,5 @@ export class AsteroidsGame {
     p5.vertex(-4, 6);
     p5.vertex(-8, 10);
     p5.endShape(p5.CLOSE);
-  }
+  };
 }
-
